43ae98adbd616c5ccf0efe7fd476d104dcdfa462
Nick Sivo <nick@kogir.com>
2016-03-02 20:00:31 -0800

    Add hacky tests.

---
+++ test/RealArtists.GitHub.TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RealArtists.GitHub.TestConsole {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using Tests;
+
+  public class Program {
+    public static void Main(string[] args) {
+      var a = new CanEven();
+      a.GetAUser("kogir").Wait();
+      a.GetAUser("james-howard").Wait();
+      Console.WriteLine("Done.");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ test/RealArtists.GitHub.Tests/CanEven.cs
@@ -0,0 +1,39 @@
+﻿namespace RealArtists.GitHub.Tests {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Net;
+  using System.Net.Http;
+  using System.Threading.Tasks;
+  using Newtonsoft.Json.Linq;
+  using Xunit;
+
+  public class CanEven : IDisposable {
+    public GitHubClient _client;
+
+    public CanEven() {
+      _client = new GitHubClient();
+    }
+
+    public void Dispose() {
+      _client.Dispose();
+    }
+
+    [Theory]
+    [InlineData("kogir")]
+    [InlineData("james-howard")]
+    public async Task GetAUser(string login) {
+      var request = new GitHubRequest(HttpMethod.Get, $"/users/{login}");
+      var response = await _client.MakeRequest<JToken>(request);
+
+      Assert.Equal(response.Status, HttpStatusCode.OK);
+      Assert.Null(response.Error);
+      Assert.NotNull(response.RateLimit);
+      Assert.Null(response.Redirect);
+      Assert.NotNull(response.ETag);
+      Assert.NotNull(response.LastModified);
+      Assert.NotNull(response.Result);
+      Assert.Equal(response.Result["login"].ToString(), login);
+    }
+  }
+}
