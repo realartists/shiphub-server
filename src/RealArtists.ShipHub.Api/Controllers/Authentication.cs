a95a039d603f5791404f44d94d4c064547720134
Nick Sivo <nick@kogir.com>
2016-03-03 22:17:12 -0800

    Dump octokit.net.

--- src/RealArtists.ShipHub.Api/Controllers/Authentication.cs
+++ src/RealArtists.ShipHub.Api/Controllers/Authentication.cs
@@ -10,7 +10,6 @@
   using Microsoft.AspNetCore.Authorization;
   using Microsoft.AspNetCore.Mvc;
   using Microsoft.Extensions.Options;
-  using Octokit;
 
   [AllowAnonymous]
   [Route("api/[controller]")]
@@ -35,82 +34,75 @@
     }
 
     [HttpPost("login")]
-    public async Task<IActionResult> Login(string accessToken, string clientName) {
-      if (string.IsNullOrWhiteSpace(accessToken)) {
-        return BadRequest($"{nameof(accessToken)} is required.");
+    public async Task<IActionResult> Login(string applicationId, string code, string state, string clientName) {
+      if (string.IsNullOrWhiteSpace(applicationId)) {
+        return BadRequest($"{nameof(applicationId)} is required.");
+      }
+      if (string.IsNullOrWhiteSpace(code)) {
+        return BadRequest($"{nameof(code)} is required.");
+      }
+      if (string.IsNullOrWhiteSpace(state)) {
+        return BadRequest($"{nameof(state)} is required.");
+      }
+      if (string.IsNullOrWhiteSpace(clientName)) {
+        return BadRequest($"{nameof(clientName)} is required.");
       }
 
-      try {
-        var appClient = _ghOpts.CreateApplicationClient();
-        var appAuth = await appClient.Authorization.CheckApplicationAuthentication(_ghOpts.ApplicationId, accessToken);
-
-        // Check scopes
-        var missingScopes = _requiredOauthScopes.Except(appAuth.Scopes).ToArray();
-        if (missingScopes.Any()) {
-          return Error("Insufficient access granted.", HttpStatusCode.Unauthorized, new {
-            MissingScopes = missingScopes,
-          });
-        }
-
-        var userClient = _ghOpts.CreateUserClient(appAuth.Token);
-        var userInfo = await userClient.User.Current();
-        var apiInfo = userClient.GetLastApiInfo();
-
-        var account = await _ghContext.Accounts
-          .Include(x => x.AuthenticationToken)
-          .SingleOrDefaultAsync(x => x.Id == userInfo.Id);
-        if (account == null) {
-          account = _ghContext.Accounts.Create();
-          account.Id = userInfo.Id;
-        }
-        account.AvatarUrl = userInfo.AvatarUrl;
-        account.Company = userInfo.Company ?? "";
-        account.CreatedAt = userInfo.CreatedAt;
-        account.Login = userInfo.Login;
-        account.Name = userInfo.Name;
-        account.ETag = apiInfo.Etag;
-
-        if (account.AuthenticationToken == null) {
-          account.AuthenticationToken = _ghContext.AuthenticationTokens.Add(new GitHubAuthenticationTokenModel() {
-            Account = account,
-          });
-        }
-        account.AuthenticationToken.AccessToken = accessToken;
-        account.AuthenticationToken.Scopes = appAuth.ScopesDelimited;
-
-        await _ghContext.SaveChangesAsync();
-
-        return Ok();
-      } catch {
-        // TODO: Scope this more tightly
-        return Unauthorized();
-      }
-    }
-
-    [HttpGet("begin")]
-    public IActionResult BeginGitHubOauth() {
-      var uri = new UriBuilder(Uri.UriSchemeHttps, Request.Host.Host, Request.Host.Port ?? 443, Url.Action($"{nameof(EndGitHubOauth)}"));
-
-      var request = new OauthLoginRequest(_ghOpts.ApplicationId) {
-        RedirectUri = uri.Uri,
-        State = "TODO: this",
-      };
-
-      foreach (var scope in _requiredOauthScopes.Union(_optionalOauthScopes)) {
-        request.Scopes.Add(scope);
+      var creds = _ghOpts.Credentials.SingleOrDefault(x => x.Id.Equals(applicationId, StringComparison.OrdinalIgnoreCase));
+      if (creds == null) {
+        return Error($"Unknown applicationId: {applicationId}", HttpStatusCode.NotFound);
       }
 
-      var client = _ghOpts.CreateApplicationClient();
-      var url = client.Oauth.GetGitHubLoginUrl(request).ToString();
-      return Redirect(url);
-    }
+      var appClient = _ghOpts.CreateApplicationClient(creds.Id);
+      var tokenInfo = await appClient.CreateAccessToken(creds.Id, creds.Secret, code, state);
+      if (tokenInfo.Error != null) {
+        return Error("Unable to retrieve token.", tokenInfo.Status, tokenInfo.Error);
+      }
+      var appAuth = tokenInfo.Result;
 
-    [HttpGet("end")]
-    public async Task<IActionResult> EndGitHubOauth(string code, string state) {
-      var client = _ghOpts.CreateApplicationClient();
-      var token = await client.Oauth.CreateAccessToken(
-        new OauthTokenRequest(_ghOpts.ApplicationId, _ghOpts.ApplicationSecret, code));
-      return Json(token);
+      // Check scopes
+      var scopes = appAuth.Scope.Split(',');
+      var missingScopes = _requiredOauthScopes.Except(scopes).ToArray();
+      if (missingScopes.Any()) {
+        return Error("Insufficient access granted.", HttpStatusCode.Unauthorized, new {
+          MissingScopes = missingScopes,
+        });
+      }
+
+      var userClient = _ghOpts.CreateUserClient(appAuth.AccessToken);
+      var userInfo = await userClient.AuthenticatedUser();
+      if (userInfo.Error != null) {
+        return Error("Unable to retrieve user information.", HttpStatusCode.Unauthorized, userInfo.Error);
+      }
+      var user = userInfo.Result;
+
+      var account = await _ghContext.Accounts
+        .Include(x => x.AuthenticationToken)
+        .SingleOrDefaultAsync(x => x.Id == user.Id);
+      if (account == null) {
+        account = _ghContext.Accounts.Create();
+        account.Id = user.Id;
+      }
+      account.AvatarUrl = user.AvatarUrl;
+      account.Company = user.Company ?? "";
+      account.CreatedAt = user.CreatedAt;
+      account.Login = user.Login;
+      account.Name = user.Name;
+      account.ETag = userInfo.ETag;
+      account.LastModified = userInfo.LastModified;
+      account.LastRefresh = DateTimeOffset.UtcNow;
+
+      if (account.AuthenticationToken == null) {
+        account.AuthenticationToken = _ghContext.AuthenticationTokens.Add(new GitHubAuthenticationTokenModel() {
+          Account = account,
+        });
+      }
+      account.AuthenticationToken.AccessToken = appAuth.AccessToken;
+      account.AuthenticationToken.Scopes = appAuth.Scope;
+
+      await _ghContext.SaveChangesAsync();
+
+      return Ok();
     }
   }
 }
