b28bf4fecdd1cb5de312171585ef9ff62eb1187f
Nick Sivo <nick@kogir.com>
2016-05-11 19:57:38 -0700

    Indexed join columns perform better. Who'd have guessed.

--- RealArtists.ShipHub.Database/User Defined Types/AccountStubTableType.sql
+++ RealArtists.ShipHub.Database/User Defined Types/AccountStubTableType.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TYPE [dbo].[AccountStubTableType] AS TABLE (
-  [AccountId] INT            NOT NULL,
+  [AccountId] INT            NOT NULL PRIMARY KEY CLUSTERED,
   [Type]      NVARCHAR(4)    NOT NULL,
   [Login]     NVARCHAR(255)  NOT NULL
 )
