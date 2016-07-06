41d48dab6c5615a80f2c9933b3f70bf7b65dd1c2
Nick Sivo <nick@kogir.com>
2016-07-06 16:13:53 -0600

    Collect and send change notifications.

--- RealArtists.ShipHub.Common/DataModel/Types/ChangeSummary.cs
+++ RealArtists.ShipHub.Common/DataModel/Types/ChangeSummary.cs
@@ -1,12 +1,18 @@
 ﻿namespace RealArtists.ShipHub.Common.DataModel.Types {
   using System.Collections.Generic;
 
-  public class ChangeSummary {
+  public interface IChangeSummary {
+    IEnumerable<long> Organizations { get; }
+    IEnumerable<long> Repositories { get; }
+  }
+
+  public class ChangeSummary : IChangeSummary {
     private HashSet<long> _orgs = new HashSet<long>();
     private HashSet<long> _repos = new HashSet<long>();
 
     public IEnumerable<long> Organizations { get { return _orgs; } }
     public IEnumerable<long> Repositories { get { return _repos; } }
+    public bool Empty { get { return _orgs.Count == 0 && _repos.Count == 0; } }
 
     public void Add(long? organizationId, long? repositoryId) {
       if (organizationId != null) {
@@ -16,6 +22,11 @@
       if (repositoryId != null) {
         _repos.Add(repositoryId.Value);
       }
+    }
+
+    public void UnionWith(IChangeSummary other) {
+      _orgs.UnionWith(other.Organizations);
+      _repos.UnionWith(other.Repositories);
     }
   }
 }
