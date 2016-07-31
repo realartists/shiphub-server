d6451763bfc507b96f6744edbf47772c22751b6a
Nick Sivo <nick@kogir.com>
2016-07-30 22:11:01 -0600

    I prefer enums but they're too brittle.
    GitHub can't be trusted.

--- RealArtists.ShipHub.Common/GitHub/Models/IssueEvent.cs
+++ RealArtists.ShipHub.Common/GitHub/Models/IssueEvent.cs
@@ -1,14 +1,13 @@
 ﻿namespace RealArtists.ShipHub.Common.GitHub.Models {
   using System;
   using System.Collections.Generic;
-  using System.Runtime.Serialization;
   using Newtonsoft.Json;
   using Newtonsoft.Json.Linq;
 
   public class IssueEvent {
     public long Id { get; set; }
     public Account Actor { get; set; }
-    public IssueEventType Event { get; set; }
+    public string Event { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public Account Assignee { get; set; }
     public Account Assigner { get; set; } // This replaces Actor for assigned events
@@ -52,114 +51,5 @@
   public class IssueRename {
     public string From { get; set; }
     public string To { get; set; }
-  }
-
-  public enum IssueEventType {
-    /// <summary>
-    /// Not a valid value. Uninitialized or an error.
-    /// </summary>
-    Unspecified = 0,
-
-    /// <summary>
-    /// The issue was closed by the actor. When the commit_id is present, it identifies the commit that closed the issue using "closes / fixes #NN" syntax.
-    /// </summary>
-    [EnumMember(Value = "closed")]
-    Closed,
-
-    /// <summary>
-    /// The issue was reopened by the actor.
-    /// </summary>
-    [EnumMember(Value = "reopened")]
-    Reopened,
-
-    /// <summary>
-    /// The actor subscribed to receive notifications for an issue.
-    /// </summary>
-    [EnumMember(Value = "subscribed")]
-    Subscribed,
-
-    /// <summary>
-    /// The issue was merged by the actor. The `commit_id` attribute is the SHA1 of the HEAD commit that was merged.
-    /// </summary>
-    [EnumMember(Value = "merged")]
-    Merged,
-
-    /// <summary>
-    /// The issue was referenced from a commit message. The `commit_id` attribute is the commit SHA1 of where that happened.
-    /// </summary>
-    [EnumMember(Value = "referenced")]
-    Referenced,
-
-    /// <summary>
-    /// The actor was @mentioned in an issue body.
-    /// </summary>
-    [EnumMember(Value = "mentioned")]
-    Mentioned,
-
-    /// <summary>
-    /// The issue was assigned to the actor.
-    /// </summary>
-    [EnumMember(Value = "assigned")]
-    Assigned,
-
-    /// <summary>
-    /// The actor was unassigned from the issue.
-    /// </summary>
-    [EnumMember(Value = "unassigned")]
-    Unassigned,
-
-    /// <summary>
-    /// A label was added to the issue.
-    /// </summary>
-    [EnumMember(Value = "labeled")]
-    Labeled,
-
-    /// <summary>
-    /// A label was removed from the issue.
-    /// </summary>
-    [EnumMember(Value = "unlabeled")]
-    Unlabeled,
-
-    /// <summary>
-    /// The issue was added to a milestone.
-    /// </summary>
-    [EnumMember(Value = "milestoned")]
-    Milestoned,
-
-    /// <summary>
-    /// The issue was removed from a milestone.
-    /// </summary>
-    [EnumMember(Value = "demilestoned")]
-    Demilestoned,
-
-    /// <summary>
-    /// The issue title was changed.
-    /// </summary>
-    [EnumMember(Value = "renamed")]
-    Renamed,
-
-    /// <summary>
-    /// The issue was locked by the actor.
-    /// </summary>
-    [EnumMember(Value = "locked")]
-    Locked,
-
-    /// <summary>
-    /// The issue was unlocked by the actor.
-    /// </summary>
-    [EnumMember(Value = "unlocked")]
-    Unlocked,
-
-    /// <summary>
-    /// The pull request's branch was deleted.
-    /// </summary>
-    [EnumMember(Value = "head_ref_deleted")]
-    HeadRefDeleted,
-
-    /// <summary>
-    /// The pull request's branch was restored
-    /// </summary>
-    [EnumMember(Value = "head_ref_restored")]
-    HeadRefRestored
   }
 }
--- RealArtists.ShipHub.Common/GitHub/Models/TimelineEvent.cs
+++ RealArtists.ShipHub.Common/GitHub/Models/TimelineEvent.cs
@@ -1,22 +1,16 @@
 ﻿namespace RealArtists.ShipHub.Common.GitHub.Models {
   using System;
   using System.Collections.Generic;
-  using System.Runtime.Serialization;
   using Newtonsoft.Json;
   using Newtonsoft.Json.Linq;
 
   public class TimelineEvent {
-    private static readonly HashSet<string> _issueEventTypeNames = new HashSet<string>(Enum.GetNames(typeof(IssueEvent)));
-
     public long Id { get; set; }
     public Account Actor { get; set; }
-    public TimelineEventType Event { get; set; }
+    public string Event { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public Account Assignee { get; set; }
     //public Milestone Milestone { get; set; } // What GitHub sends is incomplete and nearly useless, often just the name. No need to parse.
-
-    // Because now they're mingled
-    public bool IsIssueEvent { get { return _issueEventTypeNames.Contains(Event.ToString()); } }
 
     // We want these to be saved in _extensionData, so don't actually deserialize them.
     [JsonIgnore]
@@ -68,138 +62,5 @@
 
     [JsonProperty("url")]
     public string IssueUrl { get; set; }
-  }
-
-  public enum TimelineEventType {
-    /// <summary>
-    /// Not a valid value. Uninitialized or an error.
-    /// </summary>
-    Unspecified = 0,
-
-    /// <summary>
-    /// The issue was assigned to the actor.
-    /// </summary>
-    [EnumMember(Value = "assigned")]
-    Assigned,
-
-    /// <summary>
-    /// The issue was closed by the actor. When the commit_id is present, it identifies the commit that closed the issue using "closes / fixes #NN" syntax.
-    /// </summary>
-    [EnumMember(Value = "closed")]
-    Closed,
-
-    /// <summary>
-    /// A comment was added to the issue.
-    /// </summary>
-    [EnumMember(Value = "commented")]
-    Commented,
-
-    /// <summary>
-    /// A commit was added to the pull request's `HEAD` branch. Only provided for pull requests.
-    /// </summary>
-    [EnumMember(Value = "committed")]
-    Committed,
-
-    /// <summary>
-    /// The issue was referenced from another issue. The `source` attribute contains the `id`, `actor`, and `url` of the reference's source.
-    /// </summary>
-    [EnumMember(Value = "cross-referenced")]
-    CrossReferenced,
-
-    /// <summary>
-    /// The issue was removed from a milestone.
-    /// </summary>
-    [EnumMember(Value = "demilestoned")]
-    Demilestoned,
-
-    /// <summary>
-    /// The pull request's branch was deleted.
-    /// </summary>
-    [EnumMember(Value = "head_ref_deleted")]
-    HeadRefDeleted,
-
-    /// <summary>
-    /// The pull request's branch was restored
-    /// </summary>
-    [EnumMember(Value = "head_ref_restored")]
-    HeadRefRestored,
-
-    /// <summary>
-    /// A label was added to the issue.
-    /// </summary>
-    [EnumMember(Value = "labeled")]
-    Labeled,
-
-    /// <summary>
-    /// The issue was locked by the actor.
-    /// </summary>
-    [EnumMember(Value = "locked")]
-    Locked,
-
-    /// <summary>
-    /// The actor was @mentioned in an issue body.
-    /// </summary>
-    [EnumMember(Value = "mentioned")]
-    Mentioned,
-
-    /// <summary>
-    /// The issue was merged by the actor. The `commit_id` attribute is the SHA1 of the HEAD commit that was merged.
-    /// </summary>
-    [EnumMember(Value = "merged")]
-    Merged,
-
-    /// <summary>
-    /// The issue was added to a milestone.
-    /// </summary>
-    [EnumMember(Value = "milestoned")]
-    Milestoned,
-
-    /// <summary>
-    /// The issue was referenced from a commit message. The `commit_id` attribute is the commit SHA1 of where that happened.
-    /// </summary>
-    [EnumMember(Value = "referenced")]
-    Referenced,
-
-    /// <summary>
-    /// The issue title was changed.
-    /// </summary>
-    [EnumMember(Value = "renamed")]
-    Renamed,
-
-    /// <summary>
-    /// The issue was reopened by the actor.
-    /// </summary>
-    [EnumMember(Value = "reopened")]
-    Reopened,
-
-    /// <summary>
-    /// The actor subscribed to receive notifications for an issue.
-    /// </summary>
-    [EnumMember(Value = "subscribed")]
-    Subscribed,
-
-    /// <summary>
-    /// The actor was unassigned from the issue.
-    /// </summary>
-    [EnumMember(Value = "unassigned")]
-    Unassigned,
-
-    /// <summary>
-    /// A label was removed from the issue.
-    /// </summary>
-    [EnumMember(Value = "unlabeled")]
-    Unlabeled,
-
-    /// <summary>
-    /// The issue was unlocked by the actor.
-    /// </summary>
-    [EnumMember(Value = "unlocked")]
-    Unlocked,
-
-    /// <summary>
-    /// The actor unsubscribed to stop receiving notifications for an issue.
-    /// </summary>
-    [EnumMember(Value = "unsubscribed")]
-    Unsubscribed,
   }
 }
