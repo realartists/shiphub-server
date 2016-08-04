4616a192cfd84ba8ef08021206e5034c347175b6
Nick Sivo <nick@kogir.com>
2016-08-04 16:26:52 -0600

    Tools to help test logging inside WebJobs

---
+++ RealArtists.ShipHub.QueueProcessor/Test.cs
@@ -0,0 +1,19 @@
+﻿namespace RealArtists.ShipHub.QueueProcessor {
+  using System;
+  using System.Diagnostics.CodeAnalysis;
+  using System.IO;
+  using System.Threading.Tasks;
+  using Microsoft.Azure.WebJobs;
+
+  public static class Test {
+    public static async Task Echo([ServiceBusTrigger("test-echo")] string message, TextWriter logger) {
+      await logger.WriteLineAsync(message);
+    }
+
+    [SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes", Justification = "It's for testing logging. Who cares?")]
+    public static void Exception([ServiceBusTrigger("test-exception")] string message, TextWriter logger) {
+      logger.WriteLine($"[Test Exception]: {message}");
+      throw new Exception(message);
+    }
+  }
+}
