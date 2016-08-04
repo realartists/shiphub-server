3d973f57ded6fed578c2e9ce36871071344b893f
Nick Sivo <nick@kogir.com>
2016-08-04 17:42:10 -0600

    Add reactions table and table type.

---
+++ RealArtists.ShipHub.Database/Tables/Reactions.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Reactions]  (
+  [Id]           BIGINT         NOT NULL,
+  [UserId]       BIGINT         NOT NULL,
+  [IssueId]      BIGINT         NOT NULL,
+  [CommentId]    BIGINT         NULL,
+  [Content]      NVARCHAR(15)   NOT NULL,
+  [CreatedAt]    DATETIMEOFFSET NOT NULL,
+  CONSTRAINT [PK_Reactions] PRIMARY KEY CLUSTERED ([Id]),
+  CONSTRAINT [FK_Reactions_UserId_Accounts_Id] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Accounts] ([Id]),
+  CONSTRAINT [FK_Reactions_IssueId_Issues_Id] FOREIGN KEY ([IssueId]) REFERENCES [dbo].[Issues] ([Id]),
+  CONSTRAINT [FK_Reactions_CommentId_Comments_Id] FOREIGN KEY ([CommentId]) REFERENCES [dbo].[Comments] ([Id]),
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Reactions_UserId] ON [dbo].[Reactions]([UserId])
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Reactions_IssueId] ON [dbo].[Reactions]([IssueId])
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Reactions_CommentId]
+  ON [dbo].[Reactions]([CommentId])
+  WHERE ([CommentId] IS NOT NULL)
+GO
---
+++ RealArtists.ShipHub.Database/User Defined Types/ReactionTableType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[ReactionTableType] AS TABLE (
+  [Id]           BIGINT         NOT NULL,
+  [UserId]       BIGINT         NOT NULL,
+  [IssueId]      BIGINT         NOT NULL,
+  [CommentId]    BIGINT         NULL,
+  [Content]      NVARCHAR(15)   NOT NULL,
+  [CreatedAt]    DATETIMEOFFSET NOT NULL
+)
