afc0ba24249439f35b2c32c7637ff1179f8cca8b
Nick Sivo <nick@kogir.com>
2016-08-23 16:32:44 -0600

    New table hints for label creation
    Tests strategies from #105 to prevent unique key violations

--- RealArtists.ShipHub.Database/Procedures/BulkCreateLabels.sql
+++ RealArtists.ShipHub.Database/Procedures/BulkCreateLabels.sql
@@ -6,7 +6,7 @@ BEGIN
   -- interfering with SELECT statements.
   SET NOCOUNT ON
 
-  INSERT INTO Labels WITH (SERIALIZABLE) (Color, Name)
+  INSERT INTO Labels WITH (UPDLOCK SERIALIZABLE) (Color, Name)
   SELECT DISTINCT l.Color, l.Name
   FROM @Labels as l
   WHERE NOT EXISTS (SELECT * FROM Labels WHERE Color = l.Color AND Name = l.Name)
