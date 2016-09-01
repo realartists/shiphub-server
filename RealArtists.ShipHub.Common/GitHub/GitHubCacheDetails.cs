a4be043533189e8ee57d9fee4f634c7c34decd8f
Nick Sivo <nick@kogir.com>
2016-08-31 21:31:18 -0600

    Add new empty cache static.

--- RealArtists.ShipHub.Common/GitHub/GitHubCacheDetails.cs
+++ RealArtists.ShipHub.Common/GitHub/GitHubCacheDetails.cs
@@ -10,10 +10,21 @@
   }
 
   public class GitHubCacheDetails : IGitHubCacheDetails {
+    public static IGitHubCacheDetails Empty { get; } = new EmptyGitHubCacheDetails();
+
     public string AccessToken { get; set; }
     public string ETag { get; set; }
     public DateTimeOffset? LastModified { get; set; }
     public DateTimeOffset? Expires { get; set; }
     public TimeSpan PollInterval { get; set; }
+
+    // Hidden from public view...
+    private class EmptyGitHubCacheDetails : IGitHubCacheDetails {
+      public string AccessToken { get { return null; } }
+      public string ETag { get { return null; } }
+      public DateTimeOffset? Expires { get { return null; } }
+      public DateTimeOffset? LastModified { get { return null; } }
+      public TimeSpan PollInterval { get { return TimeSpan.Zero; } }
+    }
   }
 }
