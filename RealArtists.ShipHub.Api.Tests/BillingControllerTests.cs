710356c094b9a26faf0208c3328864986d150843
Fred Potter <fred@realartists.com>
2016-09-16 00:22:35 -0700

    `/billing/accounts` provides info for app subscriptions window
    A GET of `/biling/accounts` will provide a list of all accounts to be
    shown in the client subscriptions window.  i.e., your own account and
    any orgs you belong to.
    
    For each account or org, an "action" field indicates what the allowable
    action is for this account.  When action = purchase, show a Purchase
    button.  The account does not have an existing subscription.  When
    action = manage, show a Manage button.  The account has an existing
    subscription.
    
    Later we'll probably want to restrict the "manage" action to only the
    person who bought the subscription or other org admins, so we'll need a
    third action like "none_already_subscribed" or something.
    
    This endpoint will return an error response if we've failed to sync the
    subscription state from ChargeBee for all of these accounts.  If this
    happens, we wouldn't know whether to show a manage or puchase option, so
    let's just show nothing at all.

---
+++ RealArtists.ShipHub.Api.Tests/BillingControllerTests.cs
@@ -0,0 +1,87 @@
+﻿namespace RealArtists.ShipHub.Api.Tests {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Net.Http;
+  using System.Text;
+  using System.Threading.Tasks;
+  using System.Web.Http;
+  using System.Web.Http.Controllers;
+  using System.Web.Http.Hosting;
+  using System.Web.Http.Results;
+  using System.Web.Http.Routing;
+  using Common.DataModel;
+  using Controllers;
+  using Filters;
+  using NUnit.Framework;
+
+  [TestFixture]
+  [AutoRollback]
+  public class BillingControllerTests {
+
+    [Test]
+    public async Task CanGetAccounts() {
+      using (var context = new ShipHubContext()) {
+        var user = TestUtil.MakeTestUser(context);
+        user.Token = Guid.NewGuid().ToString();
+        var org1 = TestUtil.MakeTestOrg(context, 6001, "myorg1");
+        var org2 = TestUtil.MakeTestOrg(context, 6002, "myorg2");
+
+        await context.SetOrganizationUsers(org1.Id, new[] {
+          Tuple.Create(user.Id, false),
+        });
+        await context.SetOrganizationUsers(org2.Id, new[] {
+          Tuple.Create(user.Id, false),
+        });
+
+        context.Subscriptions.Add(new Subscription() {
+          AccountId = user.Id,
+          State = SubscriptionState.InTrial,
+        });
+        context.Subscriptions.Add(new Subscription() {
+          AccountId = org1.Id,
+          State = SubscriptionState.Subscribed,
+        });
+
+        await context.SaveChangesAsync();
+
+        var controller = new BillingController();
+        controller.RequestContext.Principal = new ShipHubPrincipal(user.Id, user.Login, user.Token);
+
+        var result = (OkNegotiatedContentResult<List<BillingController.Account>>)(await controller.Accounts());
+        Assert.AreEqual(2, result.Content.Count);
+        Assert.AreEqual(user.Id, result.Content[0].Id);
+        Assert.AreEqual(BillingController.AccountAction.Purchase, result.Content[0].Action,
+          "should be purchase since user is in trial");
+        Assert.AreEqual(org1.Id, result.Content[1].Id);
+        Assert.AreEqual(BillingController.AccountAction.Manage, result.Content[1].Action,
+          "should be manage since we have a subscription");
+      }
+    }
+
+    [Test]
+    public async Task AccountsReturnsErrorWithNoSubscriptionInfo() {
+      using (var context = new ShipHubContext()) {
+        var user = TestUtil.MakeTestUser(context);
+        user.Token = Guid.NewGuid().ToString();
+        var org1 = TestUtil.MakeTestOrg(context, 6001, "myorg1");
+        var org2 = TestUtil.MakeTestOrg(context, 6002, "myorg2");
+
+        await context.SetOrganizationUsers(org1.Id, new[] {
+          Tuple.Create(user.Id, false),
+        });
+        await context.SetOrganizationUsers(org2.Id, new[] {
+          Tuple.Create(user.Id, false),
+        });
+
+        await context.SaveChangesAsync();
+
+        var controller = new BillingController();
+        controller.RequestContext.Principal = new ShipHubPrincipal(user.Id, user.Login, user.Token);
+
+        var result = await controller.Accounts();
+        Assert.IsInstanceOf<NegotiatedContentResult<Dictionary<string, string>>>(result);
+      }
+    }
+  }
+}
---
+++ RealArtists.ShipHub.Api/Controllers/BillingController.cs
@@ -0,0 +1,69 @@
+﻿namespace RealArtists.ShipHub.Api.Controllers {
+  using System.Collections.Generic;
+  using System.Data.Entity;
+  using System.Linq;
+  using System.Net;
+  using System.Runtime.Serialization;
+  using System.Threading.Tasks;
+  using System.Web.Http;
+  using Common.DataModel;
+  using Filters;
+
+  [RoutePrefix("billing")]
+  public class BillingController : ShipHubController {
+
+    public enum AccountAction {
+      [EnumMember(Value = "manage")]
+      Manage,
+
+      [EnumMember(Value = "purchase")]
+      Purchase,
+    }
+
+    public class Account {
+      public long Id { get; set; }
+      public AccountAction Action { get; set; }
+    }
+
+    [HttpGet]
+    [Route("accounts")]
+    public async Task<IHttpActionResult> Accounts() {
+      var principal = RequestContext.Principal as ShipHubPrincipal;
+
+      var combined = new List<Common.DataModel.Account>();
+
+      var user = await Context.Users
+        .Include(x => x.Subscription)
+        .SingleAsync(x => x.Id == principal.UserId);
+      if (user.Subscription != null) {
+        combined.Add(user);
+      }
+
+      var orgs = await Context.OrganizationAccounts
+        .Include(x => x.Organization.Subscription)
+        .Where(x => x.UserId == principal.UserId && x.Organization.Subscription != null)
+        .Select(x => x.Organization)
+        .OrderBy(x => x.Login)
+        .ToArrayAsync();
+      combined.AddRange(orgs);
+
+      var result = combined
+        .Select(x => new Account() {
+          Id = x.Id,
+          Action = x.Subscription.State == SubscriptionState.Subscribed ? AccountAction.Manage : AccountAction.Purchase,
+        })
+        .ToList();
+
+      if (result.Count > 0) {
+        return Ok(result);
+      } else {
+        // We found no Subscription records in the db for these accounts, which can only mean
+        // that we're not correctly sync'ing with ChargeBee.  In that case, let's just show an
+        // error.
+        return Content(HttpStatusCode.ServiceUnavailable, new Dictionary<string, string>() {
+          { "message", "Subscription info for your accounts have not loaded yet. Try again later." },
+        });
+      }
+    }
+  }
+}
