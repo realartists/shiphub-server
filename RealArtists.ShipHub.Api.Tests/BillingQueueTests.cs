75001c78af92a9e1bfb3c86b083fbd37e1b9a8e7
Fred Potter <fred@realartists.com>
2016-09-21 15:24:08 -0700

    extract ShimChargeBeeWebApi into its own class
    Need to use this in another place.

--- RealArtists.ShipHub.Api.Tests/BillingQueueTests.cs
+++ RealArtists.ShipHub.Api.Tests/BillingQueueTests.cs
@@ -31,59 +31,6 @@
       return new BillingQueueHandler(new DetailedExceptionLogger());
     }
 
-    /// <summary>
-    /// ChargeBee's client library is not easily tested.  Instead of trying to shim all
-    /// of its things, we'll use this to watch outgoing HTTP requests and return fake
-    /// responses.
-    /// </summary>
-    private static void ShimChargeBeeWebApi(Func<string, string, Dictionary<string, string>, object> callback) {
-      Dictionary<object, MemoryStream> streams = new Dictionary<object, MemoryStream>();
-
-      // Workaround to make the request body of HttpWebRequest inspectable after
-      // it has been written to.
-      ShimWebRequest.CreateString = (string url) => {
-        HttpWebRequest req = (HttpWebRequest)ShimsContext.ExecuteWithoutShims(() => WebRequest.Create(url));
-        var shim = new ShimHttpWebRequest(req) {
-          // Force a MemoryStream to be returned.  Otherwise, we won't be able
-          // to inspect the request body (it's normally write-only).
-          GetRequestStream = () => {
-            if (!streams.ContainsKey(req)) {
-              streams[req] = new MemoryStream();
-            }
-            return streams[req];
-          },
-        };
-        return shim;
-      };
-
-      ApiConfig.Configure("fake-site-name", "fake-site-key");
-      ShimApiUtil.SendRequestHttpWebRequestHttpStatusCodeOut =
-        (HttpWebRequest req, out HttpStatusCode code) => {
-          NameValueCollection nvc;
-
-          if (req.Method.Equals("POST")) {
-            var stream = req.GetRequestStream();
-            stream.Position = 0;
-            string body;
-            using (var reader = new StreamReader(stream)) {
-              body = reader.ReadToEnd();
-            }
-            nvc = HttpUtility.ParseQueryString(body);
-          } else {
-            nvc = HttpUtility.ParseQueryString(req.RequestUri.Query);
-          }
-
-          var data = new Dictionary<string, string>();
-          foreach (var key in nvc.AllKeys) {
-            data[key] = nvc[key];
-          }
-
-          code = HttpStatusCode.OK;
-          object result = callback(req.Method, req.RequestUri.AbsolutePath, data);
-          return JToken.FromObject(result).ToString(Newtonsoft.Json.Formatting.Indented);
-        };
-    }
-
     [Test]
     public async Task WillCreateTrialIfNeeded() {
       using (var context = new ShipHubContext()) {
@@ -125,7 +72,7 @@
         bool setSubscriptionToCancel = false;
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
@@ -250,7 +197,7 @@
           });
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
@@ -331,7 +278,7 @@
           });
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
@@ -405,7 +352,7 @@
           });
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
@@ -488,7 +435,7 @@
         bool setSubscriptionToCancel = false;
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
@@ -601,7 +548,7 @@
           });
 
         using (ShimsContext.Create()) {
-          ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
+          ChargeBeeTestUtil.ShimChargeBeeWebApi((string method, string path, Dictionary<string, string> data) => {
             if (method.Equals("GET") && path.Equals("/api/v2/customers")) {
               Assert.AreEqual(new Dictionary<string, string>() { { "id[is]", $"user-{user.Id}" } }, data);
               // Pretend no existing customers found for this id.
