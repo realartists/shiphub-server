97f8d67bd3e6cce7b51590ed97c3436866bef188
Fred Potter <fred@realartists.com>
2016-11-10 12:53:08 -0800

    silence CA1819:PropertiesShouldNotReturnArrays on mail messages
    I just need a byte[].  I promise not to manipulate its state.  The
    recommended fix is too ugly and involves creating extra getter methods
    and adding a new ivar to hold the byte[].

--- RealArtists.ShipHub.Mail/Models/PaymentSucceededPersonalMailMessage.cs
+++ RealArtists.ShipHub.Mail/Models/PaymentSucceededPersonalMailMessage.cs
@@ -5,6 +5,7 @@
     public double AmountPaid { get; set; }
     public string LastCardDigits { get; set; }
     public DateTimeOffset InvoiceDate { get; set; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays")]
     public byte[] InvoicePdfBytes { get; set; }
     public DateTimeOffset ServiceThroughDate { get; set; }
   }
--- RealArtists.ShipHub.Mail/Models/PersonalPurchaseMailMessage.cs
+++ RealArtists.ShipHub.Mail/Models/PersonalPurchaseMailMessage.cs
@@ -4,6 +4,7 @@
   public class PurchasePersonalMailMessage : MailMessageBase {
     public bool BelongsToOrganization { get; set; }
     public bool WasGivenTrialCredit { get; set; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays")]
     public byte[] InvoicePdfBytes { get; set; }
     public DateTimeOffset InvoiceDate { get; set; }
   }
