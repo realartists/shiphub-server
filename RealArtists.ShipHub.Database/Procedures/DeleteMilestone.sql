58c65e8bd674e76ed584bec9cf27f03a90b7b228
Nick Sivo <nick@kogir.com>
2016-11-18 13:15:53 -0700

    Only mark the deleted milestone as deleted.

--- RealArtists.ShipHub.Database/Procedures/DeleteMilestone.sql
+++ RealArtists.ShipHub.Database/Procedures/DeleteMilestone.sql
@@ -7,16 +7,16 @@ BEGIN
   -- interfering with SELECT statements.
   SET NOCOUNT ON
 
-  DELETE FROM Milestones
-  WHERE Id = @MilestoneId
+  DELETE FROM Milestones WHERE Id = @MilestoneId
 
   IF(@@ROWCOUNT > 0)
   BEGIN
     UPDATE RepositoryLog WITH (UPDLOCK SERIALIZABLE) SET
-    [Delete] = 1,
-    [RowVersion] = DEFAULT
-    FROM RepositoryLog
-    WHERE RepositoryId = @RepositoryId AND [Type] = 'milestone'
+      [Delete] = 1,
+      [RowVersion] = DEFAULT
+    WHERE RepositoryId = @RepositoryId
+      AND [Type] = 'milestone'
+      AND ItemId = @MilestoneId
 
     -- Return repository if updated
     SELECT NULL as OrganizationId, @RepositoryId as RepositoryId, NULL as UserId
