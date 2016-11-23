d099c4f8aea51d14d43988a674adf343165f888e
Nick Sivo <nick@kogir.com>
2016-11-23 00:30:08 -0700

    Changing labels changes the issue.
    This wasn't your mistake. It's been wrong. May be part of #203

--- RealArtists.ShipHub.Api.Tests/WebhookTests.cs
+++ RealArtists.ShipHub.Api.Tests/WebhookTests.cs
@@ -828,8 +828,10 @@
       issue.Labels = issue.Labels.Where(x => !x.Name.Equals("Red"));
       changeSummary = await ChangeSummaryFromIssuesHook(IssueChange("unlabeled", issue, testRepo.Id), "repo", testRepo.Id, testHook.Secret.ToString());
 
-      // Expect null if there are no changes to notify about.
-      Assert.Null(changeSummary);
+      // Adding or removing a label changes the issue
+      Assert.NotNull(changeSummary, "should have generated change notification");
+      Assert.AreEqual(0, changeSummary.Organizations.Count());
+      Assert.AreEqual(new long[] { testRepo.Id }, changeSummary.Repositories.ToArray());
 
       using (var context = new Common.DataModel.ShipHubContext()) {
         var updatedIssue = context.Issues.Where(x => x.Id == testIssue.Id).First();
@@ -843,8 +845,10 @@
       issue.Labels = new Label[] { };
       changeSummary = await ChangeSummaryFromIssuesHook(IssueChange("unlabeled", issue, testRepo.Id), "repo", testRepo.Id, testHook.Secret.ToString());
 
-      // Expect null if there are no changes to notify about.
-      Assert.Null(changeSummary);
+      // Adding or removing a label changes the issue
+      Assert.NotNull(changeSummary, "should have generated change notification");
+      Assert.AreEqual(0, changeSummary.Organizations.Count());
+      Assert.AreEqual(new long[] { testRepo.Id }, changeSummary.Repositories.ToArray());
 
       using (var context = new Common.DataModel.ShipHubContext()) {
         var updatedIssue = context.Issues.Where(x => x.Id == testIssue.Id).First();
