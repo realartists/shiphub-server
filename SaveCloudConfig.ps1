9273cd0359f53b22769bf54f580c9fa17879050c
Nick Sivo <nick@kogir.com>
2016-11-25 22:52:41 -0700

    Use the output file path directly.

--- SaveCloudConfig.ps1
+++ SaveCloudConfig.ps1
@@ -13,6 +13,4 @@ $deployment = Get-AzureDeployment -ServiceName $ServiceName
 
 $configuration = [xml]$deployment.Configuration
 
-$saveFile = Join-Path $pwd $Output
-
-$configuration.Save($saveFile)
+$configuration.Save($Output)
