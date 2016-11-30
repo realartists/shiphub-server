ad286561b13fd55cfc4fbea0c2dfafbbc4c30b8f
Nick Sivo <nick@kogir.com>
2016-11-29 21:08:08 -0700

    Simplify worker implementation and lifecycle.
    May reduce deployment time.

--- RealArtists.ShipHub.CloudServices.OrleansSilos/WorkerRole.cs
+++ RealArtists.ShipHub.CloudServices.OrleansSilos/WorkerRole.cs
@@ -14,12 +14,12 @@ namespace RealArtists.ShipHub.CloudServices.OrleansSilos {
   using Orleans.Runtime.Configuration;
   using Orleans.Runtime.Host;
 
+  // For lifecycle details see https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-role-lifecycle-dotnet
   [SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable")]
   public class WorkerRole : RoleEntryPoint {
-    private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
-
+    private bool _abort;
     private AzureSilo _silo;
-    private CancellationTokenSource _cancellationTokenSource;
+    private ManualResetEvent _stopped = new ManualResetEvent(false);
 
     public override bool OnStart() {
       Log.Trace();
@@ -45,84 +45,68 @@ namespace RealArtists.ShipHub.CloudServices.OrleansSilos {
       // For information on handling configuration changes
       // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
       RoleEnvironment.Changing += RoleEnvironmentChanging;
+      AppDomain.CurrentDomain.UnhandledException +=
+        (object sender, UnhandledExceptionEventArgs e) => {
+          var ex = e.ExceptionObject as Exception;
+          if (ex != null) {
+            Log.Exception(ex, "Unhandled exception in Orleans CloudServices Worker Role.");
+          }
+        };
 
       return base.OnStart();
     }
 
+    [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
     public override void Run() {
       Log.Trace();
 
-      try {
-        using (_cancellationTokenSource = new CancellationTokenSource()) {
-          Log.Info("Running worker.");
-          runCompleteEvent.Reset();
-          RunAsync(_cancellationTokenSource.Token).Wait();
-          Log.Info("Worker exited.");
+      while (!_abort) {
+        try {
+          var shipHubConfig = new ShipHubCloudConfiguration();
+          var siloConfig = AzureSilo.DefaultConfiguration();
+
+          // This allows App Services and Cloud Services to agree on a deploymentId.
+          siloConfig.Globals.DeploymentId = shipHubConfig.DeploymentId;
+
+          // Dependency Injection
+          siloConfig.UseStartupType<SimpleInjectorProvider>();
+
+          siloConfig.AddMemoryStorageProvider();
+          siloConfig.AddAzureTableStorageProvider("AzureStore", shipHubConfig.DataConnectionString);
+
+          // It is IMPORTANT to start the silo not in OnStart but in Run.
+          // Azure may not have the firewalls open yet (on the remote silos) at the OnStart phase.
+          _silo = new AzureSilo();
+          _silo.Start(siloConfig);
+
+          // Block until silo is shutdown
+          _silo.Run();
+        } catch (Exception e) {
+          Log.Exception(e, "Error while running silo. Restarting within Run method.");
         }
-      } catch (Exception e) {
-        Log.Exception(e);
-        throw;
-      } finally {
-        runCompleteEvent.Set();
-        _cancellationTokenSource = null;
       }
+      _stopped.Set();
     }
 
     public override void OnStop() {
       Log.Trace();
 
-      _cancellationTokenSource?.Cancel();
-
-      var silo = _silo;
-      _silo = null;
-
-      if (silo != null) {
+      _abort = true;
+      if (_silo != null) {
         Log.Info("Stopping silo.");
-        silo.Stop();
-
-        Log.Info("Waiting for worker to stop.");
-        runCompleteEvent.WaitOne();
-        Log.Info("Worker stopped.");
-      } else {
-        Log.Info("Silo is null. Wat?");
+        _silo.Stop();
+        Log.Info("Stopped silo.");
       }
 
-      RoleEnvironment.Changing -= RoleEnvironmentChanging;
+      _stopped.WaitOne();
 
       base.OnStop();
-    }
-
-    private Task RunAsync(CancellationToken cancellationToken) {
-      while (!cancellationToken.IsCancellationRequested) {
-        var shipHubConfig = new ShipHubCloudConfiguration();
-        var siloConfig = AzureSilo.DefaultConfiguration();
-
-        // This allows App Services and Cloud Services to agree on a deploymentId.
-        siloConfig.Globals.DeploymentId = shipHubConfig.DeploymentId;
-
-        // Dependency Injection
-        siloConfig.UseStartupType<SimpleInjectorProvider>();
-
-        siloConfig.AddMemoryStorageProvider();
-        siloConfig.AddAzureTableStorageProvider("AzureStore", shipHubConfig.DataConnectionString);
-
-        // It is IMPORTANT to start the silo not in OnStart but in Run.
-        // Azure may not have the firewalls open yet (on the remote silos) at the OnStart phase.
-        var silo = new AzureSilo();
-        silo.Start(siloConfig);
-        _silo = silo;
-
-        // Block until silo is shutdown
-        silo.Run();
-      }
-
-      return Task.CompletedTask;
     }
 
     private static void RoleEnvironmentChanging(object sender, RoleEnvironmentChangingEventArgs e) {
       int i = 1;
       foreach (var c in e.Changes) {
-        Trace.WriteLine(string.Format("RoleEnvironmentChanging: #{0} Type={1} Change={2}", i++, c.GetType().FullName, c));
+        Log.Info(string.Format("RoleEnvironmentChanging: #{0} Type={1} Change={2}", i++, c.GetType().FullName, c));
       }
 
       // If a configuration setting is changing);
