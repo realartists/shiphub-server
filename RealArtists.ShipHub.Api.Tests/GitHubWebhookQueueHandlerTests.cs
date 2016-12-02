80bbcccc55c1203f133a2c5da028b184d1193f30
Fred Potter <fred@realartists.com>
2016-12-02 11:44:36 -0800

    break WebhookTests into two files
    ... one for the api controller, another for the queue handler.

--- RealArtists.ShipHub.Api.Tests/GitHubWebhookQueueHandlerTests.cs
+++ RealArtists.ShipHub.Api.Tests/GitHubWebhookQueueHandlerTests.cs
@@ -4,16 +4,10 @@
   using System.Linq;
   using System.Net;
   using System.Net.Http;
-  using System.Runtime.Remoting.Metadata.W3cXsd2001;
   using System.Security.Cryptography;
   using System.Text;
   using System.Threading;
   using System.Threading.Tasks;
-  using System.Web.Http;
-  using System.Web.Http.Controllers;
-  using System.Web.Http.Hosting;
-  using System.Web.Http.Results;
-  using System.Web.Http.Routing;
   using ActorInterfaces;
   using AutoMapper;
   using Common;
@@ -34,14 +28,7 @@
 
   [TestFixture]
   [AutoRollback]
-  public class WebhookTests {
-
-    private static string SignatureForPayload(string key, string payload) {
-      var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(key));
-      byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(payload));
-      return "sha1=" + new SoapHexBinary(hash).ToString();
-    }
-
+  public class GitHubWebhookQueueHandlerTests {
     private static IMapper AutoMapper { get; } = CreateMapper();
 
     private static IMapper CreateMapper() {
@@ -69,24 +56,6 @@
 
       var mapper = config.CreateMapper();
       return mapper;
-    }
-
-    private static void ConfigureController(ApiController controller, string eventName, JObject body, string secretKey) {
-      var json = JsonConvert.SerializeObject(body, GitHubSerialization.JsonSerializerSettings);
-      var signature = SignatureForPayload(secretKey, json);
-
-      var config = new HttpConfiguration();
-      var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost/webhook");
-      request.Headers.Add("User-Agent", GitHubWebhookController.GitHubUserAgent);
-      request.Headers.Add(GitHubWebhookController.EventHeaderName, eventName);
-      request.Headers.Add(GitHubWebhookController.SignatureHeaderName, signature);
-      request.Headers.Add(GitHubWebhookController.DeliveryIdHeaderName, Guid.NewGuid().ToString());
-      request.Content = new ByteArrayContent(Encoding.UTF8.GetBytes(json));
-      var routeData = new HttpRouteData(config.Routes.MapHttpRoute("Webhook", "webhook"));
-
-      controller.ControllerContext = new HttpControllerContext(config, routeData, request);
-      controller.Request = request;
-      controller.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = config;
     }
 
     private static JObject IssueChange(string action, Issue issue, long repositoryId) {
@@ -138,43 +107,6 @@
         Events = "event1,event2",
         OrganizationId = orgId,
       });
-    }
-
-    [Test]
-    public async Task QueueHookWillQueueWebhookEvent() {
-      using (var context = new Common.DataModel.ShipHubContext()) {
-        var obj = JObject.FromObject(new {
-          hook_id = 1234,
-          repository = new {
-            id = 5001,
-          },
-        }, GitHubSerialization.JsonSerializer);
-
-        GitHubWebhookEventMessage message = null;
-        var mockBusClient = new Mock<IShipHubQueueClient>();
-        mockBusClient.Setup(x => x.QueueWebhookEvent(It.IsAny<GitHubWebhookEventMessage>()))
-          .Returns((GitHubWebhookEventMessage arg) => {
-            message = arg;
-            return Task.CompletedTask;
-          });
-
-        var controller = new GitHubWebhookController(mockBusClient.Object);
-        ConfigureController(controller, "ping", obj, "somesecret");
-        var result = await controller.QueueHook("repo", 5001);
-        Assert.IsInstanceOf(typeof(OkResult), result);
-
-        var expectedJson = JsonConvert.SerializeObject(obj, GitHubSerialization.JsonSerializerSettings);
-        var hmac = new HMACSHA1(Encoding.UTF8.GetBytes("somesecret"));
-        byte[] expectedSignature = hmac.ComputeHash(Encoding.UTF8.GetBytes(expectedJson));
-        
-        Assert.NotNull(message, "Should have queued a message to process the webhook event");
-        Assert.AreEqual(5001, message.EntityId);
-        Assert.AreEqual("repo", message.EntityType);
-        Assert.AreEqual(expectedSignature, message.Signature);
-        Assert.AreEqual("ping", message.EventName);
-        Assert.NotNull(message.DeliveryId);
-        Assert.AreEqual(expectedJson, message.Payload);
-      }
     }
 
     private GitHubWebhookEventMessage CreateMessage(Common.DataModel.Hook hook, string eventName, JObject payload) {
