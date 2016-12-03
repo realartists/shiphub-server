d9aa003c363bf99af4cbf41db7bff248b4e1028b
Nick Sivo <nick@kogir.com>
2016-12-03 13:17:27 -0700

    Enable testing on Azure

---
+++ RealArtists.ShipHub.Api.Tests/PrepareAzureTest.ps1
@@ -0,0 +1,6 @@
+﻿$old_string = "Server=(local);Database=ShipHubTest;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=true"
+$new_string = "Server=tcp:shiphub-dev-dbserver.database.windows.net,1433;Persist Security Info=False;Database=shiphub-dev-test;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+Get-Children *.config -Recurse | ForEach {
+(Get-Content $_ | ForEach {$_ -replace $old_string, $new_string}) | Set-Content $_
+}
