a99461e5b72b414a2536d890006e5c0d716712d3
Nick Sivo <nick@kogir.com>
2016-12-03 13:52:59 -0700

    Secret Regex feature is the suck.

--- RealArtists.ShipHub.Api.Tests/PrepareAzureTest.ps1
+++ RealArtists.ShipHub.Api.Tests/PrepareAzureTest.ps1
@@ -1,6 +1,12 @@
 ﻿$old_string = "Server=(local);Database=ShipHubTest;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=true"
 $new_string = "Server=tcp:shiphub-dev-dbserver.database.windows.net,1433;Persist Security Info=False;Database=shiphub-dev-test;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
-Get-ChildItem *.config -Recurse | ForEach {
-(Get-Content $_ | ForEach {$_ -replace $old_string, $new_string}) | Set-Content $_
+Write-Host "Original: $old_string"
+Write-Host "Replacement: $new_string"
+Write-Host "Working Directory: $PSScriptRoot"
+
+Get-ChildItem *.config -Recurse -Path $PSScriptRoot | ForEach {
+  $fullPath = $_.FullName
+  Write-Host $fullPath
+  (Get-Content $fullPath | ForEach {$_ -replace [regex]::escape($old_string), $new_string}) | Set-Content $fullPath
 }
