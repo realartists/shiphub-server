d217cd293308863611f29cfcc421ee0790fbc2bb
Nick Sivo <nick@kogir.com>
2016-12-26 21:18:30 -0700

    See if we can run tests.

---
+++ RealArtists.ShipHub.Api.Tests/PrepareAppVeyorTest.ps1
@@ -0,0 +1,12 @@
+﻿$old_string = "Server=(local);Database=ShipHubTest;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=true"
+$new_string = "Server=(local)\SQL2016;Database=shiphub-dev-test;User=ShipUser;Password=uB4vtZbsjUGvqzmS0S6i;MultipleActiveResultSets=True;Connection Timeout=30;"
+
+Write-Host "Original: $old_string"
+Write-Host "Replacement: $new_string"
+Write-Host "Working Directory: $PSScriptRoot"
+
+Get-ChildItem *.config -Recurse -Path $PSScriptRoot | ForEach {
+  $fullPath = $_.FullName
+  Write-Host $fullPath
+  (Get-Content $fullPath | ForEach {$_ -replace [regex]::escape($old_string), $new_string}) | Set-Content $fullPath
+}
