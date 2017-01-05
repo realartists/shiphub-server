691a16fe52b8a26855881b45b8a99a965407854a
Nick Sivo <nick@kogir.com>
2017-01-04 23:46:15 -0700

    For good measure and to kick off a deployment.

--- RealArtists.ShipHub.Common/Constants.cs
+++ RealArtists.ShipHub.Common/Constants.cs
@@ -3,6 +3,6 @@
 
   public static class Constants {
     public static long ServerVersion { get; } = 2;
-    public static Guid PurgeIdentifier { get; } = new Guid("00000000-0000-0000-0000-000000000006");
+    public static Guid PurgeIdentifier { get; } = new Guid("00000000-0000-0000-0000-000000000007");
   }
 }
