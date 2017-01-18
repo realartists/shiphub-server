3d1ef1a1553b0b295c872489689d4e698e710dcd
Nick Sivo <nick@kogir.com>
2017-01-17 18:03:17 -0700

    Purge

--- RealArtists.ShipHub.Common/Constants.cs
+++ RealArtists.ShipHub.Common/Constants.cs
@@ -3,6 +3,6 @@
 
   public static class Constants {
     public static long ServerVersion { get; } = 2;
-    public static Guid PurgeIdentifier { get; } = new Guid("00000000-0000-0000-0000-000000000007");
+    public static Guid PurgeIdentifier { get; } = new Guid("00000000-0000-0000-0000-000000000008");
   }
 }
