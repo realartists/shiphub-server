78504889b763e07805469d1b021e361f062ed74b
Nick Sivo <nick@kogir.com>
2017-01-24 20:00:54 -0700

    Fix a PK violation recording usage.

--- RealArtists.ShipHub.Database/Procedures/RecordUsage.sql
+++ RealArtists.ShipHub.Database/Procedures/RecordUsage.sql
@@ -7,9 +7,14 @@ BEGIN
   -- interfering with SELECT statements.
   SET NOCOUNT ON
 
-  INSERT INTO Usage (AccountId, [Date])
-  SELECT @AccountId, @Date
-  WHERE NOT EXISTS (
-    SELECT * FROM Usage
-    WHERE AccountId = @AccountId AND [Date] = @Date)
+  MERGE INTO Usage WITH (SERIALIZABLE) as [Target]
+  USING (
+    SELECT @AccountId as AccountId, @Date as [Date]
+  ) as [Source]
+  ON ([Target].AccountId = [Source].AccountId AND [Target].[Date] = [Source].[Date])
+  -- Add
+  WHEN NOT MATCHED BY TARGET THEN
+    INSERT (AccountId, [Date])
+    VALUES (AccountId, [Date])
+  OPTION (LOOP JOIN, FORCE ORDER);
 END
