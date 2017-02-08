97771e5266e94aa544362f6dae787b44a560e35e
Nick Sivo <nick@kogir.com>
2017-02-07 19:33:04 -0700

    Send notifications

--- RealArtists.ShipHub.Actors/RepositoryActor.cs
+++ RealArtists.ShipHub.Actors/RepositoryActor.cs
@@ -707,7 +707,7 @@
             await context.BulkUpdateHooks(hooks: new[] { editHook });
           } else if (editResponse.Status == HttpStatusCode.NotFound) {
             // Our record is out of date.
-            await context.BulkUpdateHooks(deleted: new[] { editHook.Id });
+            changes = await context.BulkUpdateHooks(deleted: new[] { editHook.Id });
           } else {
             throw new Exception($"Failed to edit hook for repo '{_fullName}' ({_repoId}): {editResponse.Status} {editResponse.Error}");
           }
