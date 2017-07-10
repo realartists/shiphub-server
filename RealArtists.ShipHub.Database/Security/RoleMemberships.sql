f60858de89c5f69654fe4dfeedbea8b1251bd7ea
Nick Sivo <nick@kogir.com>
2017-07-10 20:56:26 +0000

    Add StateOnly user.
    For viewing metrics.

--- RealArtists.ShipHub.Database/Security/RoleMemberships.sql
+++ RealArtists.ShipHub.Database/Security/RoleMemberships.sql
@@ -1,8 +1,16 @@
 ﻿ALTER ROLE [db_owner] ADD MEMBER [ShipUser]
 GO
 
+-- ReadOnly User
 ALTER ROLE [db_datareader] ADD MEMBER [ReadOnly]
 GO
 
 ALTER ROLE [db_denydatawriter] ADD MEMBER [ReadOnly]
+GO
+
+-- StateOnly User
+ALTER ROLE [db_denydatareader] ADD MEMBER [StateOnly]
+GO
+
+ALTER ROLE [db_denydatawriter] ADD MEMBER [StateOnly]
 GO
