274b6108376909f556b93083a7159b1e9db583b0
Nick Sivo <nick@kogir.com>
2017-08-06 19:32:51 -0600

    Split up the GitHub interfaces.
    I think this makes it less confusing? Maybe not?

---
+++ RealArtists.ShipHub.ActorInterfaces/GitHub/IGitHubPublicPoolActor.cs
@@ -0,0 +1,12 @@
+﻿namespace RealArtists.ShipHub.ActorInterfaces.GitHub {
+  using System;
+  using Orleans.CodeGeneration;
+
+  /// <summary>
+  /// Interacts with GitHub on behalf of all ship users, using randomly selected credentials.
+  /// </summary>
+  [Version(1)]
+  public interface IGitHubPublicPoolActor : Orleans.IGrainWithIntegerKey, IGitHubPoolable {
+    // Yep, nothing to do here.
+  }
+}
---
+++ RealArtists.ShipHub.ActorInterfaces/GitHub/IGitHubRepositoryAdmin.cs
@@ -0,0 +1,17 @@
+﻿namespace RealArtists.ShipHub.ActorInterfaces.GitHub {
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Common.GitHub;
+  using Common.GitHub.Models;
+
+  /// <summary>
+  /// These operations only make sense for repository administrators. 
+  /// </summary>
+  public interface IGitHubRepositoryAdmin {
+    Task<GitHubResponse<Webhook>> AddRepositoryWebhook(string repoFullName, Webhook hook, RequestPriority priority = RequestPriority.Background);
+    Task<GitHubResponse<bool>> DeleteRepositoryWebhook(string repoFullName, long hookId, RequestPriority priority = RequestPriority.Background);
+    Task<GitHubResponse<Webhook>> EditRepositoryWebhookEvents(string repoFullName, long hookId, IEnumerable<string> events, RequestPriority priority = RequestPriority.Background);
+    Task<GitHubResponse<bool>> PingRepositoryWebhook(string repoFullName, long hookId, RequestPriority priority = RequestPriority.Background);
+    Task<GitHubResponse<IEnumerable<Webhook>>> RepositoryWebhooks(string repoFullName, GitHubCacheDetails cacheOptions = null, RequestPriority priority = RequestPriority.Background);
+  }
+}
