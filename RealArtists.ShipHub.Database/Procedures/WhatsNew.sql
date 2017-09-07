cbc244e29ffb25b07be07115f9740f897a7fd680
Nick Sivo <nick@kogir.com>
2017-09-07 17:36:06 -0600

    Return IDs for deleted issue events.

--- RealArtists.ShipHub.Database/Procedures/WhatsNew.sql
+++ RealArtists.ShipHub.Database/Procedures/WhatsNew.sql
@@ -211,7 +211,7 @@ BEGIN
       AND l.ItemType = 'comment'
 
     -- Events
-    SELECT e.Id, e.RepositoryId, e.IssueId, e.ActorId, e.[Event], e.CreatedAt, e.ExtensionData, l.[Delete],
+    SELECT l.ItemId as Id, e.RepositoryId, e.IssueId, e.ActorId, e.[Event], e.CreatedAt, e.ExtensionData, l.[Delete],
            CAST(CASE WHEN a.UserId IS NULL THEN e.Restricted ELSE 0 END as BIT) as Restricted
     FROM @Logs as l
       LEFT OUTER JOIN IssueEvents as e ON (l.ItemId = e.Id)
