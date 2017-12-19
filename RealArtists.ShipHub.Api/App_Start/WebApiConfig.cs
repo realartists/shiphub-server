3f2fda63683e7b73ea705896194566d54846a690
Nick Sivo <nick@kogir.com>
2017-12-18 23:59:13 -0700

    Disable RayGun until I can audit it.
    Something is populating the request message data and needs to be
    disabled.

--- RealArtists.ShipHub.Api/App_Start/WebApiConfig.cs
+++ RealArtists.ShipHub.Api/App_Start/WebApiConfig.cs
@@ -4,14 +4,14 @@
   using System.Net.Http;
   using System.Net.Http.Formatting;
   using System.Threading.Tasks;
-  using System.Web;
+  //using System.Web;
   using System.Web.Http;
   using System.Web.Http.ExceptionHandling;
   using Common;
-  using Controllers;
+  //using Controllers;
   using Diagnostics;
   using Filters;
-  using Mindscape.Raygun4Net.WebApi;
+  //using Mindscape.Raygun4Net.WebApi;
 
   public static class WebApiConfig {
     public static void Register(HttpConfiguration config, string raygunApiKey) {
@@ -20,7 +20,7 @@
       config.Filters.Add(new AuthorizeAttribute());
       config.Filters.Add(new CommonLogActionFilterAttribute());
 
-      RaygunWebApiClient.Attach(config, RaygunClientFactory(raygunApiKey));
+      //RaygunWebApiClient.Attach(config, RaygunClientFactory(raygunApiKey));
 
       config.Formatters.Clear();
       config.Formatters.Add(new ChunkedJsonMediaTypeFormatter() { SerializerSettings = JsonUtility.JsonSerializerSettings });
@@ -35,32 +35,32 @@
       config.Services.Add(typeof(IExceptionLogger), new CommonLogExceptionLogger());
     }
 
-    public static Func<HttpRequestMessage, RaygunWebApiClient> RaygunClientFactory(string raygunApiKey) {
-      return (HttpRequestMessage requestMessage) => {
-        RaygunWebApiClient client = null;
-        if (raygunApiKey.IsNullOrWhiteSpace()) {
-          // Use default.
-          client = new RaygunWebApiClient();
-        } else {
-          client = new RaygunWebApiClient(raygunApiKey);
-        }
+    //public static Func<HttpRequestMessage, RaygunWebApiClient> RaygunClientFactory(string raygunApiKey) {
+    //  return (HttpRequestMessage requestMessage) => {
+    //    RaygunWebApiClient client = null;
+    //    if (raygunApiKey.IsNullOrWhiteSpace()) {
+    //      // Use default.
+    //      client = new RaygunWebApiClient();
+    //    } else {
+    //      client = new RaygunWebApiClient(raygunApiKey);
+    //    }
 
-        client.AddWrapperExceptions(typeof(AggregateException));
+    //    client.AddWrapperExceptions(typeof(AggregateException));
 
-        if (requestMessage.GetActionDescriptor()?.ControllerDescriptor?.ControllerType == typeof(GitHubWebhookController)) {
-          // Webhook calls from GitHub don't really have a user, but we'd still like for Raygun
-          // to show us how many unique orgs + repos are affected by a given error.  We can abuse
-          // the User concept to get this.
-          client.User = requestMessage.RequestUri.PathAndQuery.Replace('/', '_');
-        } else {
-          if (HttpContext.Current?.User is ShipHubPrincipal user) {
-            client.User = user.DebugIdentifier;
-          }
-        }
+    //    if (requestMessage.GetActionDescriptor()?.ControllerDescriptor?.ControllerType == typeof(GitHubWebhookController)) {
+    //      // Webhook calls from GitHub don't really have a user, but we'd still like for Raygun
+    //      // to show us how many unique orgs + repos are affected by a given error.  We can abuse
+    //      // the User concept to get this.
+    //      client.User = requestMessage.RequestUri.PathAndQuery.Replace('/', '_');
+    //    } else {
+    //      if (HttpContext.Current?.User is ShipHubPrincipal user) {
+    //        client.User = user.DebugIdentifier;
+    //      }
+    //    }
 
-        return client;
-      };
-    }
+    //    return client;
+    //  };
+    //}
   }
 
   public class ChunkedJsonMediaTypeFormatter : JsonMediaTypeFormatter {
