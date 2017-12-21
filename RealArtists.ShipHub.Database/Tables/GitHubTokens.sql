1eff70843759e4af96a2cb33828b931c7238a318
Nick Sivo <nick@kogir.com>
2017-12-20 18:02:10 -0700

    Fix names and formatting.
    Also force a re-build.

--- RealArtists.ShipHub.Database/Tables/GitHubTokens.sql
+++ RealArtists.ShipHub.Database/Tables/GitHubTokens.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[GitHubTokens] (
-  [Token]   NVARCHAR(64)NOT NULL,
-  [UserId]  BIGINT NOT NULL,
-  [Version] INT NOT NULL DEFAULT 1
+  [Token]   NVARCHAR(64) NOT NULL,
+  [UserId]  BIGINT       NOT NULL,
+  [Version] INT          NOT NULL DEFAULT 1
   CONSTRAINT [PK_GitHubTokens] PRIMARY KEY CLUSTERED ([Token] ASC),
   CONSTRAINT [FK_GitHubTokens_AccountId_Accounts_Id] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Accounts] ([Id])
 )
 GO
 
-CREATE NONCLUSTERED INDEX [UIX_GitHubTokens_AccountId] ON [dbo].[GitHubTokens]([UserId])
+CREATE NONCLUSTERED INDEX [IX_GitHubTokens_AccountId] ON [dbo].[GitHubTokens]([UserId])
 GO
 
-CREATE NONCLUSTERED INDEX [UIX_GitHubTokens_Version] ON [dbo].[GitHubTokens]([Version])
+CREATE NONCLUSTERED INDEX [IX_GitHubTokens_Version] ON [dbo].[GitHubTokens]([Version])
 GO
